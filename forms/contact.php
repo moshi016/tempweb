4f2d839e423a0b34ade30d1651e698ca3ffe8884
moshi016 <mdmoshi@yahoo.com>
2024-05-28 18:42:28 -0400

    update smtp

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,38 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// Replace contact@example.com with your real receiving email address
+$receiving_email_address = 'farizmoshi@gmail.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'farizmoshi@gmail.com';
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$contact->to = $receiving_email_address;
+$contact->from_name = isset($_POST['name']) ? $_POST['name'] : '';
+$contact->from_email = isset($_POST['email']) ? $_POST['email'] : '';
+$contact->subject = isset($_POST['subject']) ? $_POST['subject'] : '';
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+
+$contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'farizmoshi@gmail.com',
     'password' => 'Meerza9966Rida#',
-    'port' => '587'
-  );
-  
+    'port' => '587',
+    'encryption' => 'tls' // Use 'tls' for TLS or 'ssl' for SSL
+);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$contact->add_message($contact->from_name, 'From');
+$contact->add_message($contact->from_email, 'Email');
+$contact->add_message(isset($_POST['message']) ? $_POST['message'] : '', 'Message', 10);
 
-  echo $contact->send();
+if ($contact->send()) {
+    echo 'Message sent successfully';
+} else {
+    echo 'Failed to send message';
+}
 ?>
